ddb5c17fbbc92dc367018705984f6c78494cb95f
Wilhelm Brinster <Wilya3@yandex.ru>
2021-02-06 16:34:54 +0600

    did almost all methods in manager. Interface didnt touch. Have many todo

---
+++ SQLQueryManager.py
@@ -0,0 +1,187 @@
+
+
+class SQLQueryManager:
+    """
+    Utility class for automatically creating queries.
+    It works with databases using SQL transaction language.\n
+    Methods DON'T EXECUTE a SQL query. They only
+    create a string of query and return it.\n
+    """
+
+    def createTable(self, tableName, listOfColumnNames, listOfColumnTypesAnd, listOfColumnNamesPK):
+        """
+        # TODO: Определить, где вводятся "настройки" столбцов
+        DOESNT EXECUTE A QUERY. Only returns SQL query\n
+        It has to only create a table and set a PRIMARY KEY [NOT NULL, DEFAULT].\n
+        Example: ListOfColumnTypes: ["SERIAL", "INTEGER DEFAULT 0 NOT NULL"]\n
+        Others constraints (CHECK, UNIQUE) should be added with "addConstraint()"\n
+        :param String tableName:\n
+        :param list listOfColumnNames:\n
+        :param list listOfColumnTypes:\n
+        :param list listOfColumnNamesPK:\n
+        :return String query:
+        """
+        if len(listOfColumnNames) != len(listOfColumnTypes):
+            raise ValueError("Number of columns not equals number of types")
+        # TODO: Исправить проверку! Добавить проверку на типы в дочернем методе
+        for PKName in listOfColumnNamesPK:
+            if PKName in listOfColumnNames:
+                raise ValueError("Primary key column's name is invalid")
+
+        query = "CREATE TABLE " + tableName + "\n(\n"
+        for i in range(len(listOfColumnNames)):
+            query += listOfColumnNames[i] + listOfColumnTypes[i] + ",\n"
+        # TODO: Переделать место, где присваивается PRIMARY KEY
+        query += "PRIMARY KEY ("
+        for i in range(len(listOfColumnNamesPK)):
+            query += listOfColumnNamesPK[i]
+            if i != len(listOfColumnNamesPK) - 1:
+                query += ", "
+        query += ")"
+        query += "\n);"
+        return query
+
+    def dropTable(self, tableName):
+        """
+        DOESNT EXECUTE A QUERY. Only returns SQL query\n
+        Deletes a table\n
+        :param String tableName:\n
+        :return String query:
+        """
+        query = "DROP TABLE " + tableName
+
+    def addColumn(self, tableName, columnName, columnType):
+        """
+        DOESNT EXECUTE A QUERY. Only returns SQL query\n
+        Adds a new column to the table\n
+        :param String tableName:\n
+        :param String columnName: \n
+        :param String columnType: \n
+        :return String query:
+        """
+        query = "ALTER TABLE " + tableName + \
+                "\nADD COLUMN " + columnName + " " + columnType
+        return query
+
+    def dropColumn(self, tableName, columnName):
+        """
+        DOESNT EXECUTE A QUERY. Only returns SQL query\n
+        Drop a column from the table\n
+        :param String tableName:\n
+        :param String columnName: \n
+        :return String query:
+        """
+        query = "ALTER TABLE " + tableName + \
+                "\nDROP COLUMN " + columnName
+        return query
+
+    def addConstraintCheck(self, tableName, constraintName, checkCondition):
+        """
+        DOESNT EXECUTE A QUERY. Only returns SQL query\n
+        Add CHECK constraint to the table.\n
+        :param String tableName:\n
+        :param String constraintName:\n
+        :param String checkCondition:\n
+        :return String query:
+        """
+        query = "ALTER TABLE " + tableName + \
+                "\nADD CONSTRAINT " + constraintName + " CHECK (" + checkCondition + ");"
+        return query
+
+    def addConstraintUnique(self, tableName, columnName, constraintName):
+        """
+        DOESNT EXECUTE A QUERY. Only returns SQL query\n
+        Add UNIQUE constraint to the table.\n
+        :param String tableName:\n
+        :param String constraintName:\n
+        :param String columnName:\n
+        :return String query:
+        """
+        query = "ALTER TABLE " + tableName + \
+                "\nADD CONSTRAINT " + constraintName + " UNIQUE (" + columnName + ");"
+        return query
+
+    def dropConstraint(self, tableName, constraintName):
+        """
+        :param String tableName:\n
+        :param String constraintName:\n
+        :return String query:
+        """
+        query = "ALTER TABLE " + tableName + \
+                "\nDROP CONSTRAINT " + constraintName + ";"
+        return query
+
+    def addValue(self, tableName, listOfColumns, listOfValues=None):
+        """
+        TODO: Доделать везде комментарии
+        If listOfColumns is empty, then all columns to fill necessary.
+        :param String tableName:\n
+        :param list listOfColumns:\n
+        :param list listOfValues: (MAY BE EMPTY)\n
+        :return String query:
+        """
+        query = "INSERT INTO " + tableName
+        if len(listOfColumns) != 0:
+            query += " ("
+            for i in range(len(listOfColumns)):
+                query += listOfColumns[i]
+                if i != len(listOfColumns) - 1:
+                    query += ", "
+            query += ")"
+        query += "\nVALUES ("
+        for i in range(len(listOfValues)):
+            query += listOfValues[i]
+            if i != len(listOfValues) - 1:
+                query += ", "
+        query += ");"
+        return query
+
+    def changeValue(self, tableName, columnName, newValue, conditionWhere=""):
+        """
+        :return:
+        """
+        query = "UPDATE " + tableName + \
+                "\nSET " + columnName + " = " + newValue
+        if len(conditionWhere) > 0:
+            query += "\nWHERE " + conditionWhere
+        query += ";"
+
+    def deleteValue(self, tableName, conditionWhere):
+        """
+        :return:
+        """
+        query = "DELETE FROM " + tableName + \
+                "\nWHERE " + conditionWhere + ";"
+
+    def getValue(self, tableName, ):
+        """
+        :return:
+        """
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    # SUKAAAAAAAAAAAAAAAA BLYAT NADOELO YZHE
+    # def createDB(self, name):
+    #     # query = "CREATE DATABASE " + str(name)
+    #     pass
--- ThirdLabSQLInterface.py
+++ ThirdLabSQLInterface.py
@@ -1,16 +1,15 @@
+# TODO: Сравнить сигнатуры с менеджером, когда доделаю его
 
 
-class SQLQueryManager:
+class SQLInterface:
     """
-    Interface for automatically creating queries.
-    It works with databases using SQL transaction language.\n
-    Methods DON'T EXECUTE a SQL query. They only
-    create a string of query and return it.\n
+    Interface for classes which works with DB.
+    It works with databases using SQL.\n
+    # TODO: Изменить конструктор для наследования
     It HAS TO make cursor of external library
     for DBMS while creating object.
     """
 
-    # TODO: Изменить конструктор для наследования
     def __init__(self, name, user, password, host):
         self.cursor = None
 
@@ -24,99 +23,82 @@ class SQLQueryManager:
 
     def createTable(self, tableName, listOfColumnNames, listOfColumnTypes, listOfColumnNamesPK):
         """
-        DOESNT EXECUTE A QUERY. Only returns SQL query\n
-        It has to only create a table and set a PRIMARY KEY.\n
+        ABSTRACT METHOD.
+        # TODO: Пересмотреть, куда засунуть NOT NULL и DEFAULT
+        It has to only create a table and set a PRIMARY KEY [NOT NULL, DEFAULT].\n
         Example: ListOfColumnTypes: ["SERIAL", "INTEGER DEFAULT 0 NOT NULL"]\n
         Others constraints (CHECK, UNIQUE) should be added with "addConstraint()"\n
         :param String tableName:\n
         :param list listOfColumnNames:\n
         :param list listOfColumnTypes:\n
         :param list listOfColumnNamesPK:\n
-        :return String query:
         """
-        if len(listOfColumnNames) != len(listOfColumnTypes):
-            raise ValueError("Number of columns not equals number of types")
-        # TODO: Исправить проверку! Добавить проверку на типы в дочернем методе
-        for PKName in listOfColumnNamesPK:
-            if PKName in listOfColumnNames:
-                raise ValueError("Primary key column's name is invalid")
-
-        query = "CREATE TABLE " + tableName + "\n(\n"
-        for i in range(len(listOfColumnNames)):
-            query += listOfColumnNames[i] + listOfColumnTypes[i] + ",\n"
-
-        query += "PRIMARY KEY ("
-        for i in range(len(listOfColumnNamesPK)):
-            query += listOfColumnNamesPK[i]
-            if i != len(listOfColumnNamesPK) - 1:
-                query += ", "
-        query += ")"
-        query += "\n);"
-        return query
+        pass
 
     def dropTable(self, tableName):
         """
-        DOESNT EXECUTE A QUERY. Only returns SQL query\n
         Deletes a table\n
         :param String tableName:\n
-        :return String query:
         """
-        query = "DROP TABLE " + tableName
+        pass
 
     def addColumn(self, tableName, columnName, columnType):
         """
-        DOESNT EXECUTE A QUERY. Only returns SQL query\n
         Adds a new column to the table\n
         :param String tableName:\n
         :param String columnName: \n
         :param String columnType: \n
-        :return String query:
         """
-        query = "ALTER TABLE " + tableName + "\nADD COLUMN " + columnName + " " + columnType
-        return query
+        pass
 
     def dropColumn(self, tableName, columnName):
         """
-        DOESNT EXECUTE A QUERY. Only returns SQL query\n
         Drop a column from the table\n
-        :return String query:
+        :param String tableName:\n
+        :param String columnName: \n
         """
-        query = "ALTER TABLE " + tableName + "\nDROP COLUMN " + columnName
-        return query
+        pass
 
-    def addConstraintCheck(self, tableName, constraintName, ):
+    def addConstraintCheck(self, tableName, columnName):
         """
-        DOESNT EXECUTE A QUERY. Only returns SQL query\n
         Add CHECK constraint to the table.\n
-        :return String query:
+        :param String tableName:\n
+        :param String columnName: \n
         """
+        pass
 
-    def addConstraintUnique(self, tableName, constraintName, ):
+    def addConstraintUnique(self, tableName, columnName):
         """
-        DOESNT EXECUTE A QUERY. Only returns SQL query\n
         Add UNIQUE constraint to the table.\n
-        :return String query:
+        :param String tableName:\n
+        :param String columnName: \n
         """
+        pass
 
-    def dropConstraint(self):
+    def dropConstraint(self, constraintName):
         """
-        :return:
+        Delete constraint by its name.
+        :param String constraintName: \n
         """
+        pass
 
     def addValue(self):
         """
         :return:
         """
+        pass
 
     def changeValue(self):
         """
         :return:
         """
+        pass
 
     def deleteValue(self):
         """
         :return:
         """
+        pass
 
 
 
